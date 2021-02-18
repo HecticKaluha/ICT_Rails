1e8cfaee0bc585e99e0cda51d66b4168e7767e3a
Stefano <sverhoeve@davinci.nl>
2021-02-18 13:38:14 +0100

    Added migrations for entities

---
+++ app/Http/Controllers/VehicleStatusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VehicleStatus;
+use Illuminate\Http\Request;
+
+class VehicleStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\VehicleStatus  $vehicleStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(VehicleStatus $vehicleStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\VehicleStatus  $vehicleStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(VehicleStatus $vehicleStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\VehicleStatus  $vehicleStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, VehicleStatus $vehicleStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\VehicleStatus  $vehicleStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(VehicleStatus $vehicleStatus)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_02_18_122451_create_vehicle_statuses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehicleStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_statuses');
+    }
+}
